6d86195d60469d32724b750bc26a57309335dd22
Hà Minh Cường <55825733+haminhcuong2k@users.noreply.github.com>
2020-12-08 15:45:02 +0700

    add Edit User Profile template

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var footwearRouter = require('./routes/footwears');
 var loginRouter = require('./routes/user/login');
+var editRouter = require('./routes/user/edit');
 var signupRouter = require('./routes/user/signup');
 var detailRouter = require('./routes/productdetail');
 
@@ -35,6 +36,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/footwears', footwearRouter);
 app.use('/login', loginRouter);
+app.use('/edit', editRouter);
 app.use('/signup', signupRouter);
 app.use('/footwears', detailRouter);
 
---
+++ routes/user/edit.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('edit/edit', { title: "Login" });
+});
+
+router.post("/", function(req, res) {
+
+});
+module.exports = router;
