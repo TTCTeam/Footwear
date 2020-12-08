4027bcb821bf5de1d3b0477c7778e81eb400e4c7
Hà Minh Cường <55825733+haminhcuong2k@users.noreply.github.com>
2020-12-08 15:47:08 +0700

    Update edit.js

--- routes/user/edit.js
+++ routes/user/edit.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('edit/edit', { title: "Login" });
+    res.render('edit/edit', { title: "Edit" });
 });
 
 router.post("/", function(req, res) {
