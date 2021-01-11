d0779716ef54fd5c670f749e4ee7a0a3e757d7cf
DOAN MINH TAN <50938085+minhtandoan@users.noreply.github.com>
2021-01-11 21:32:36 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/TTCTeam/Footwear""
    This reverts commit 2aca13a6d31f015345353a4501d7ade2f5c77983.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,27 @@
+const passport = require('../passport');
+
 var express = require('express');
 var router = express.Router();
 const footweawrController = require('../controllers/footwearsController');
+const e = require('express');
 
-/* GET home page. */
-/* router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Footwear' });
-}); */
+function loggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        if (req.user.status == "active") {
+            console.log('Chua active');
+            res.redirect('/users/active');
 
+        } else {
+            return next();
+        }
+    } else {
+        res.redirect('/users/login');
+    }
+}
 router.get('/', footweawrController.index);
 router.get('/about', footweawrController.about);
 router.get('/contact', footweawrController.contact);
-router.get('/cart', footweawrController.cart);
+router.get('/cart', loggedIn, footweawrController.cart);
 router.get('/checkout', footweawrController.checkout);
 router.get('/men', footweawrController.men);
 router.get('/women', footweawrController.women);
